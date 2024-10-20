7ab1bab0c10ac5bc6dd1b7141fde108ffb075c11
Kaddu720 <nwilsonmalgus@gmail.com>
2024-10-19 17:12:39 -0700

    try fixing the order

--- ekiree_dashboard/ed/migrations/0001_initial.py
+++ ekiree_dashboard/ed/migrations/0001_initial.py
@@ -10,8 +10,8 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
         ('siteconfig', '0001_initial.py'),
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
     operations = [
