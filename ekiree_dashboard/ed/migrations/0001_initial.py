1c7243b3c10dce7e594c101c13993e5eb14c43cf
Kaddu720 <nwilsonmalgus@gmail.com>
2024-10-19 18:15:59 -0700

    test

--- ekiree_dashboard/ed/migrations/0001_initial.py
+++ ekiree_dashboard/ed/migrations/0001_initial.py
@@ -10,9 +10,8 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+        #migrations.swappable_dependency(settings.AUTH_USER_MODEL),
         ('siteconfig', '0001_initial'),
-        ('cow', "test")
     ]
 
     operations = [
