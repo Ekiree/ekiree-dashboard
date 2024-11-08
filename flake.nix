17087770352885a140dfbffd389c0c0846e1e65f
Kaddu720 <51177491+Kaddu720@users.noreply.github.com>
2024-11-07 21:18:00 -0800

    CU-86eqpnj2f_Clean-up--nix-flake (#15)
    * CU-86eqpnj2f_Clean-up--nix-flake
    
    * fix dependency environment

--- flake.nix
+++ flake.nix
@@ -4,15 +4,7 @@
   inputs = {
     # Nix Packages
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    flake-utils = {
-      url = "github:numtide/flake-utils";
-    };
-
-    #poetry2nix
-    poetry2nix = {
-      url = "github:nix-community/poetry2nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = {
@@ -22,8 +14,8 @@
     poetry2nix,
     ...
   }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = nixpkgs.legacyPackages.${system};
+    flake-utils.lib.eachDefaultSystem (system: 
+      let pkgs = import nixpkgs { inherit system; };
 
       # Setting up nix2poetry
       inherit
@@ -43,6 +35,7 @@
       poetryDev = mkPoetryEnv {
         projectDir = self;
         overrides = p2n-overrides;
+        extraPackages = ps: [ ps.pip ps.poetry ];
       };
 
       # Configure build dependencies for individual python packages
@@ -66,36 +59,37 @@
           )
           pypkgs-build-requirements
       );
-    in {
-      # Production Application Package
-      packages.default = poetryProd.dependencyEnv;
+    in
+      with pkgs; rec {
+        # Development shell
+        devShell = mkShell {
+          nativeBuildInputs = [
+            poetry
+            jq
+            sops
+            poetryDev
+          ];
 
-      # Development shell
-      devShells.default = pkgs.mkShell {
-        packages = [
-          pkgs.poetry
-          pkgs.jq
-          pkgs.sops
-          poetryDev
-        ];
+          # Command run upon shell start
+          shellHook = ''
+            export POETFOLIO_SECRET_KEY=$(sops  --decrypt ./secrets/secrets.json | jq -r .poetfolio_secret_key)
+            export POETFOLIO_PRODUCTION=$(sops  --decrypt ./secrets/secrets.json | jq -r .poetfolio_production)
+            export POETFOLIO_DB_NAME=$(sops  --decrypt ./secrets/secrets.json | jq -r .poetfolio_db_name)
+            export POETFOLIO_DB_USER=$(sops  --decrypt ./secrets/secrets.json | jq -r .poetfolio_db_user)
+            export POETFOLIO_DB_PASSWORD=$(sops  --decrypt ./secrets/secrets.json | jq -r .poetfolio_db_password)
 
-        # Command run upon shell start
-        shellHook = ''
-          export POETFOLIO_SECRET_KEY=$(sops  --decrypt ./secrets/secrets.json | jq -r .poetfolio_secret_key)
-          export POETFOLIO_PRODUCTION=$(sops  --decrypt ./secrets/secrets.json | jq -r .poetfolio_production)
-          export POETFOLIO_DB_NAME=$(sops  --decrypt ./secrets/secrets.json | jq -r .poetfolio_db_name)
-          export POETFOLIO_DB_USER=$(sops  --decrypt ./secrets/secrets.json | jq -r .poetfolio_db_user)
-          export POETFOLIO_DB_PASSWORD=$(sops  --decrypt ./secrets/secrets.json | jq -r .poetfolio_db_password)
+            export POETFOLIO_DB_HOST=$(sops  --decrypt ./secrets/secrets.json | jq -r .poetfolio_db_host)
+            export POETFOLIO_STATIC=$(sops  --decrypt ./secrets/secrets.json | jq -r .poetfolio_static)
+            export POETFOLIO_MEDIA=$(sops  --decrypt ./secrets/secrets.json | jq -r .poetfolio_media)
+            export POETFOLIO_EMAIL_HOST=$(sops  --decrypt ./secrets/secrets.json | jq -r .poetfolio_email_host)
+            export POETFOLIO_EMAIL_USER=$(sops  --decrypt ./secrets/secrets.json | jq -r .poetfolio_email_user)
+            export POETFOLIO_EMAIL_PASSWORD=$(sops  --decrypt ./secrets/secrets.json | jq -r .poetfolio_email_password)
 
-          export POETFOLIO_DB_HOST=$(sops  --decrypt ./secrets/secrets.json | jq -r .poetfolio_db_host)
-          export POETFOLIO_STATIC=$(sops  --decrypt ./secrets/secrets.json | jq -r .poetfolio_static)
-          export POETFOLIO_MEDIA=$(sops  --decrypt ./secrets/secrets.json | jq -r .poetfolio_media)
-          export POETFOLIO_EMAIL_HOST=$(sops  --decrypt ./secrets/secrets.json | jq -r .poetfolio_email_host)
-          export POETFOLIO_EMAIL_USER=$(sops  --decrypt ./secrets/secrets.json | jq -r .poetfolio_email_user)
-          export POETFOLIO_EMAIL_PASSWORD=$(sops  --decrypt ./secrets/secrets.json | jq -r .poetfolio_email_password)
+            export PS1="\n(develop)\[\033[1;32m\][\[\e]0;\u@\h: \w\a\]\u@\h:\w]\$\[\033[0m\] "
+          '';
+        };
 
-          export PS1="\n(develop)\[\033[1;32m\][\[\e]0;\u@\h: \w\a\]\u@\h:\w]\$\[\033[0m\] "
-        '';
-      };
-    });
+        # The default package when a specific package name isn't specified.
+        packages.default = poetryProd.dependencyEnv;
+      });
 }
